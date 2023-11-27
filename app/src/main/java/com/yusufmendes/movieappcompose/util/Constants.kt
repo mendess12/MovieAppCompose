d6da6ec9ab98fadea3fb9979c6634cfff37f5e0d
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-11-27 12:30:35 +0300

    Add constants

---
+++ app/src/main/java/com/yusufmendes/movieappcompose/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.yusufmendes.movieappcompose.util
+
+object Constants {
+    const val API_KEY = "f6175c42"
+    const val  BASE_URL = "http://www.omdbapi.com"
+    const val  IMDB_ID = "imdb_id"
+}
