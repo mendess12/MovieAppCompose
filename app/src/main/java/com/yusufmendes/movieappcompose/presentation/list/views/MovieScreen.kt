170561e3a15792a064afc728f275005d2ae1a2c1
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-11-30 20:03:32 +0300

    Add search bar

---
+++ app/src/main/java/com/yusufmendes/movieappcompose/presentation/list/views/MovieScreen.kt
@@ -0,0 +1,55 @@
+package com.yusufmendes.movieappcompose.presentation.list.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.yusufmendes.movieappcompose.presentation.list.MoviesEvent
+import com.yusufmendes.movieappcompose.presentation.list.MoviesListViewModel
+
+@Composable
+fun MovieScreen(
+    navController: NavController,
+    viewModel: MoviesListViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Column() {
+            MovieSearchBar(modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+                hint = "Batman",
+                onSearch = {
+                    viewModel.onEvent(MoviesEvent.Search(it))
+                })
+
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.movies) { movie ->
+                    Text(
+                        text = movie.title,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+
+}
